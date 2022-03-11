1b7344e2bd0d7ea961a7cd907de7da1be618a316
SadullaevTolkinjon <sadullayevtolik@gmail.com>
2022-03-12 01:29:59 +0500

    Updated

--- lib/core/utils/routers.dart
+++ lib/core/utils/routers.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mehnatkash/screens/views/get_started_page/get_started_page.dart';
+import 'package:mehnatkash/screens/views/sign_in/sign_in.dart';
 
 class RouteGenerator {
   Route? routeGenerate(RouteSettings settings) {
@@ -8,6 +9,10 @@ class RouteGenerator {
       case "/":
         return MaterialPageRoute(
           builder: (_) => const GetStartedPage(),
+        );
+      case "/sign_in":
+        return MaterialPageRoute(
+          builder: (_) => Sing_in_Page(),
         );
     }
     return null;
